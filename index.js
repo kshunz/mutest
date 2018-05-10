783bdda198fdec8b00166d2a0a0828e7a3820f71
Dwayne Ford <dwayne@bl.io>
2018-05-10 07:34:40 -0400

    return condition

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world!');
+module.exports = function test(expectedResult, conditionToTest) {
+	var fail = '\033[1;31mFail: \033[0m';
+	var pass = '\033[1;32mPass: \033[0m';
+  var failMsg = fail + `${expectedResult} expected but got ${conditionToTest}`;
+  var passMsg = pass + `${expectedResult} === ${conditionToTest}`;
+  var conditionPasses = conditionToTest === expectedResult;
+  var response = conditionPasses ? passMsg : failMsg;
+
+  process.stdout.write(response  + '\n');
+
+	return conditionPasses;
+};
